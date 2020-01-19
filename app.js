f081e6e33d84e10edc6cb466bc174742eba9ca98
Alexandre <alexandre9999lopes@gmail.com>
2020-01-19 00:32:51 -0300

    Instalação do bodyparser

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const bodyParser = require('body-parser');
+ 
 const indexRouter = require("./routes/index");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(logger("dev"));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const saveToPdf = require("../controller/safeToPdf");
+const getFinalGrades = require("../controller/getFinalGrades");
 const config = require('../config');
 
 const puppeteer = require("puppeteer");
@@ -51,68 +52,13 @@ router.get("/scraper", async (req, res, __) => {
 });
 
 // Login 
-router.get("/login", async (req, res, __) => {
+router.post("/boletim", async (req, res, __) => {
+  let login = req.query.login || '';
+  let password = req.query.password || '';
 
-  // Browser actions & buffer creator
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"] // SEE BELOW WARNING!!!
-  });
-  
-  const page = await browser.newPage();
-    
+  let grades = await getFinalGrades(login, password);
 
-  await page.setRequestInterception(true);
-  page.on('request', req => {
-        const whitelist = ['document', 'script', 'xhr', 'fetch'];
-    if (!whitelist.includes(req.resourceType())) {
-      return req.abort();
-    }
-    req.continue();
-  });
-
-
-  await page.goto('https://academico.ifgoiano.edu.br/qacademico/index.asp?t=1001', {waitUntil: 'networkidle0'});
-  
-
-  await page.focus('#txtLogin');
-  await page.keyboard.type(config.login);
-
-  await page.focus('#txtSenha');
-  await page.keyboard.type(config.password);
-
-  await page.click('#btnOk');
-
-  await page.waitForNavigation({waitUntil:'networkidle2'});
-
-  await page.click('a[href="/qacademico/alunos/boletim/index.asp"]');
-
-  const table = 'table [width="97%"] > tbody > tr:nth-child(n+3)';
-  await page.waitForSelector(table);
-
-  const disciplinas = await page.evaluate(() => {
-
-    let nomeLista = document.querySelectorAll('table [width="97%"] > tbody > tr:nth-child(n+3) > td:first-child');
-    let notaLista = document.querySelectorAll('table [width="97%"] > tbody > tr:nth-child(n+3) > td:nth-child(11n+10)');
-    let statusLista = document.querySelectorAll('table [width="97%"] > tbody > tr:nth-child(n+3) > td:last-of-type')
-    let faltasLista = document.querySelectorAll('table [width="97%"] > tbody > tr:nth-child(n+3) > td:nth-child(11n+4)')
-
-    let disciplinaDados = [];
-    for (let i = 0; i < nomeLista.length; i++) {
-      disciplinaDados[i] = {
-        nome: nomeLista[i].innerText.split('<')[0].trim(),
-        nota: notaLista[i].innerText.trim(),
-        status: statusLista[i].innerText.trim(),
-        faltas: faltasLista[i].innerText.trim()
-      };
-      
-    }
-    return disciplinaDados; 
-  });
-
-  res.json(disciplinas);
-
-  await browser.close();
-
+  res.json(grades);
 });
 
 module.exports = router;
