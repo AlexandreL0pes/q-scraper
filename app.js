378cbade4a036cfb07b4ddd14c899c26e64bc9a0
Alexandre <alexandre9999lopes@gmail.com>
2020-01-21 17:57:50 -0300

    Alteração do app

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(logger("dev"));
 app.use(express.json());
