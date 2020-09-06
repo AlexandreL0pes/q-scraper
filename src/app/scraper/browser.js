912e2d4a8a96b0926cb701f4d021add94ea57c0e
Alexandre Lopes <alexandre9999lopes@gmail.com>
2020-09-06 01:24:05 -0300

    Adição do módulo browser

---
+++ src/app/scraper/browser.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+const browser = async config => {
+  const defaultViewport = {
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    height: 1024,
+    isLandscape: false,
+    isMobile: false,
+    width: 1280,
+  };
+  const browser = await puppeteer.launch({
+    headless: true,
+    devtools: false,
+    ignoreHTTPSErrors: true,
+    slowMo: 0,
+    args: [
+      '--disable-notifications',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
+
+  const page = await browser.newPage();
+
+  await page.setViewport(defaultViewport);
+
+  return {
+    page: page,
+    close: async () => await browser.close(),
+  };
+};
+
+module.exports = browser;
