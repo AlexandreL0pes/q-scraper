b4a9b81d3371dcde8de223ec7ff6713fec6372c5
Alexandre Lopes <alexandre9999lopes@gmail.com>
2020-09-06 01:24:52 -0300

    Adição de controller

---
+++ src/app/controller/Grades.js
@@ -0,0 +1,14 @@
+const browser = require('../scraper/browser');
+const { login } = require('../scraper/scrap');
+
+modules.exports = {
+  async index(req, res) {
+    const { page } = await browser();
+
+    try {
+      
+    } catch (error) {
+      
+    }
+  },
+};
